2a2af5ddecda6042967fd066170bed53fc87ecd0
apolloyang2017 <32840924+apolloyang2017@users.noreply.github.com>
2020-01-18 13:57:20 +0800

    Update pino-mongodb.js

--- pino-mongodb.js
+++ pino-mongodb.js
@@ -39,10 +39,14 @@ function handleConnection (e, mClient) {
     var l = log(line)
     //if (program.collection == "mqttuser") {
       upsert(collection, {"clientid": l.deviceid}, {"$set":l})
-      if ((l.type == 'lockgateway') || (l.type == 'nbgateway')) {
+      if ((l.type != 'gateway') && (l.type != 'tcpgateway')) {
+        var dtype = l.type
+        if (l.type == 'lockgateway') {
+           dtype = 'lock'
+        }  
         var ts = Math.round(new Date().getTime()/1000)
         upsert(db.collection("device"), {"_deviceid": l.deviceid}, {"$set":{"_appkey": l.appkey || "iSurpassApp", "_deviceid": l.deviceid, "_isvirtual": true, "_create_ts": ts}})
-        upsert(db.collection("node"), {"_deviceid": l.deviceid, "_nodeid": 1}, {"$set":{"_appkey": l.appkey || "iSurpassApp", "_deviceid": l.deviceid, "_nodeid": 1, "_logicid": l.deviceid+"-1-0-", "_isentity": true, "_islogic": true, "_nodetype": "lock", "_logicnodetype": "lock", "_create_ts": ts}})
+        upsert(db.collection("node"), {"_deviceid": l.deviceid, "_nodeid": 1}, {"$set":{"_appkey": l.appkey || "iSurpassApp", "_deviceid": l.deviceid, "_nodeid": 1, "_logicid": l.deviceid+"-1-0-", "_isentity": true, "_islogic": true, "_nodetype": dtype, "_logicnodetype": dtype, "_create_ts": ts}})
       }
       if (l.deviceid.indexOf("iTCP") == 0) {
         var ts = Math.round(new Date().getTime()/1000)
